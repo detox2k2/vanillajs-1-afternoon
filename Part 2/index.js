db7d2378ef60e08e15ba8faeca93c401944d42c6
detox2k2 <39745373+detox2k2@users.noreply.github.com>
2019-03-20 07:30:33 -0500

    Add files via upload

---
+++ Part 2/index.js
@@ -0,0 +1,9 @@
+let id = document.getElementById('idInput');
+let color = document.getElementById('colorInput');
+
+
+const setCard = () => {
+    const card = document.getElementById(id.value)
+    
+    card.style.color = color.value
+}
